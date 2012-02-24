802e755bc127cf0edf639f61e9040ff38659dac0
Anton Zhuravsky <a.zhuravsky@itransition.com>
2012-02-24 19:05:54 +0300

    Re-ordering apps

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,5 +1,7 @@
 class PublishersController < ApplicationController
   
+  before_filter :parse_params
+  
   def show
   end
   
@@ -17,6 +19,8 @@ protected
     @ranks = GameSnapshot.where(:itunes_id => @metas.keys, :imports => {:id => Import.last}).
                           joins(:chart_snapshot => [:chart, :import]).
                           select("game_snapshots.*, charts.country as country, charts.genre as genre, charts.kind as kind").group_by(&:itunes_id)
+  
+    @sorted_itunes_id = @metas.keys.sort_by { |id| (@ranks[id] || []).map(&:rank).min }
   end
   
 end
